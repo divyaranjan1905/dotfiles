6ce4867b050269af5413c7d22a092b29190c1a5c
Divya Ranjan <divya@subvertising.org>
2024-09-17 15:23:25 +0000

    Adding gpg and ssh services to home configuration.

--- .config/guix/home-divya.scm
+++ .config/guix/home-divya.scm
@@ -1,55 +1,129 @@
-(use-modules (gnu home)
-	     (gnu home services)
-	     (gnu home services shells)
-	     (gnu services)
-	     (gnu packages)
-	     (gnu packages admin)
-	     (guix gexp))
+;;; Divya's home profile on Guix system.
+;;; This lives in ~/.config/guix/home-divya.scm and initiated through guix home reconfigure
+
+(define-module (guix-home-config)
+  #:use-module (gnu home)
+  #:use-module (guix gexp)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services gnupg)
+  #:use-module (gnu home services shepherd)
+  #:use-module (gnu home services sound)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu services)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu system shadow))
+
+
+;;; Services
+
+;; Emacs Daemon Shepherd Service
+
+;; (define (emacs-daemon-shepherd-service config)
+;;   (list (shepherd-service
+;; 	 (documentation "Emacs server.")
+;; 	 (provision '(emacs-server))
+;; 	 (start #~(make-forkec-constructor
+;; 		   (list #$ (file-append emacs "/bin/emacs")
+;; 			 "--fg-daemon")))
+;; 	 (stop #~(make-kill-destructor)))))
+
+;; (define home-emacs-daemon-service-type
+;;   (service-type
+;;    (name 'home-emacs-daemon)
+;;    (extensions (list (service-extension home-shepherd-service-type
+;; 					emacs-daemon-shepherd-service)))
+;;    (default-value #f)
+;;    (description "Emacs daemon as a Shepherd service.")))
+
 
 (home-environment
- (packages (append (map specification->package '(
-				     ;; Themes
-				     "materia-theme"
-				     "arc-icon-theme"
-				     "papirus-icon-theme"
-				     "bibata-cursor-theme"
+ (packages (append (map specification->package
+			'(
+			  ;; Themes
+			  "materia-theme"
+			  "arc-icon-theme"
+			  "papirus-icon-theme"
+			  "bibata-cursor-theme"
 
-				     ;; Fonts
-				     "font-google-noto-emoji"
+			  ;; Fonts
+			  "font-google-noto-emoji"
 
-				     ;; Audio
-				     "ardour"
-				     "mumble"
-				     "audacity"
+			  ;; Audio
+			  "ardour"
+			  "mumble"
+			  "audacity"
 
-				     ;; Reading
-				     "calibre"
+			  ;; Reading
+			  "calibre"
 
-				     ;; Research
+			  ;; Research
+			  "anki"
 
+			  ;; Emacs
+			  "emacs-pdf-tools"
+			  "emacs-saveplace-pdf-view"
 
-				     ;; Browsers
-				     "nyxt"
+			  ;; Essential R
+			  "r-dplyr"
+			  "r-tidyverse"
+			  "r-magrittr"
+			  "r-ggplot2"
 
-				     ;; Common Lisp
-				     "cl-anaphora"
-				     "cl-alexandria"
-				     "cl-asdf"
+			  ;; Browsers
+			  "nyxt"
 
-				     ;; Messaging
-				     "gnunet"
-				     "qtox"
-				     "telegram-desktop"
+			  ;; Common Lisp
+			  "cl-anaphora"
+			  "cl-alexandria"
+			  "cl-asdf"
 
-				     ;; Torrenting
-				     "qbittorrent"
-				     "rtorrent"
+			  ;; Messaging
+			  "gnunet"
+			  "qtox"
+			  "telegram-desktop"
 
-				     ;; Astrophysics
-				     "stellarium"
+			  ;; Torrenting
+			  "qbittorrent"
+			  "rtorrent"
 
-				     ;; Utilities
-				     "clipmenu"
-				     "qdirstat"
+			  ;; Astrophysics
+			  "stellarium"
 
-				     )))))
+	                  ;;; Security
+			  ;; "pinentry"
+			  ;; "pinentry-emacs"
+
+			  ;; Utilities
+			  "clipmenu"
+			  "qdirstat"
+
+			  ))))
+
+ (services (append
+	    (list
+	     (service home-gpg-agent-service-type
+                      (home-gpg-agent-configuration
+                       (pinentry-program
+			(file-append pinentry-emacs "/bin/pinentry-emacs"))
+                       (ssh-support? #t)
+                       (default-cache-ttl 3000)
+                       (max-cache-ttl 6000)
+                       (extra-content "\
+          allow-loopback-pinentry
+          allow-emacs-pinentry\n")))
+
+	     (service home-openssh-service-type
+		      (home-openssh-configuration
+		       (hosts
+			(list (openssh-host (name "git.sr.ht")
+					    (user "divyaranjan"))
+			      ))))
+	     (service home-dbus-service-type)
+	     (service home-pipewire-service-type
+		      (home-pipewire-configuration
+		       (wireplumber wireplumber-minimal)))))))
