3ea671102c3f4d8bb27b51aec6fd62329ab27d76
Divya Ranjan <divya@subvertising.org>
2024-11-08 12:27:17 +0000

    Change the module name

--- .config/guix/packages/audio.scm
+++ .config/guix/packages/audio.scm
@@ -1,68 +1,83 @@
 ;;; Divya’s Guix audio or sound related packages
 
-(define-module (divya-packages-audio)
-  #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix build utils)
-  #:use-module (guix build-system meson)
-  #:use-module (guix licenses)
-  #:use-module (guix gexp)
-
-  #:use-module (gnu packages pkg-config)
+(define-module (audio)
+  #:use-module (gnu packages audio)
   #:use-module (gnu packages commencement)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
-  #:use-module (gnu packages gettext)
-  #:use-module (gnu packages audio)
-  #:use-module (gnu packages glib)
-  #:use-module (gnu packages gtk))
+
+  #:use-module (guix build utils)
+  #:use-module (guix build-system meson)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix licenses)
+  #:use-module (guix packages)
+  #:use-module (guix utils))
 
 (define-public jack-mixer
   (package
     (name "jack-mixer")
     (version "19")
-    (source (origin
-	      (method url-fetch)
-	      (uri (string-append "https://github.com/jack-mixer/jack_mixer/releases/download/release-"
-				  version
-				  "/jack_mixer-"
-				  version
-				  ".tar.xz"))
-	      (sha256
-	       (base32
-		"059v4r5avh67p402kwr47vb2k5hl3fpj4kfkk75b0mhry36by338"))))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/jack-mixer/jack_mixer")
+             (commit (string-append "release-" version))))
+       (sha256
+        (base32 "18m6a9asbwaslw418i2w04kgc6jgdpw01i3kawdqy903kw66hnhj"))))
     (build-system meson-build-system)
     (arguments
      (list
       #:build-type "release"
       #:phases #~(modify-phases %standard-phases
-		   (add-after 'unpack 'path-patch
-		     (lambda _
-		       (substitute* '("meson.build")
-			 (("'/', 'etc', 'xdg'")
-			  (string-append "'" #$output "'"))))))
-      ))
-    (native-inputs
-     (list gcc-toolchain
-	   pkg-config
-	   python
-	   python-cython
-	   python-docutils
-	   gnu-gettext
-	   glib
-	   gtk+
-	   `(,gtk+ "bin")
-	   ))
-    (inputs
-     (list python
-	   python-pygobject
-	   python-pycairo
-	   python-platformdirs
-	  jack-2))
-   (synopsis "JACK Mixer: A multi-channel audio mixer desktop application for the JACK Audio Connection Kit.")
-   (description
-    "jack_mixer is a GTK+ JACK audio mixer app with a look & handling similar to hardware mixing desks. It has lot of useful features, apart from being able to mix multiple JACK audio streams.")
-   (home-page "https://rdio.space/jackmixer/")
-   (license gpl2+)))
+                   (add-after 'unpack 'path-patch
+                     (lambda _
+                       (substitute* '("meson.build")
+                         (("'/', 'etc', 'xdg'")
+                          (string-append "'"
+                                         #$output "'")))))
+                   (add-after 'install 'wrap-path
+                     (lambda* (#:key outputs #:allow-other-keys)
+                       (let* ((out (assoc-ref outputs "out"))
+                              (bin (string-append out "/bin/"))
+			      (gi-typelib-path (getenv "GI_TYPELIB_PATH"))
+                              (version #$(version-major+minor (package-version
+                                                               (this-package-input
+                                                                "python"))))
+                              (lib (string-append out "/lib/python" version
+                                                  "/site-packages")))
+                         (wrap-program (string-append bin "jack_mixer")
+                           `("GUIX_PYTHONPATH" ":" prefix
+                             (,(getenv "GUIX_PYTHONPATH") ,lib))
+			   `("GI_TYPELIB_PATH" ":" prefix (,gi-typelib-path)))))))))
+    (native-inputs (list gcc-toolchain
+                         pkg-config
+                         python-cython
+                         python-docutils
+                         gnu-gettext
+                         glib))
+    (propagated-inputs (list gtk+
+			     `(,gtk+ "bin")
+			     python
+			     bash-minimal
+			     python-wrapper
+			     python-pygobject
+			     python-pycairo
+			     python-platformdirs
+			     jack-2))
+    (synopsis
+     "JACK Mixer: A multi-channel audio mixer for the JACK Audio Connection Kit")
+    (description
+     "The jack_mixer is a GTK+ JACK audio mixer app with a look & handling
+similar to hardware mixing desks.  It has lot of useful features, apart
+from being able to mix multiple JACK audio streams.")
+    (home-page "https://rdio.space/jackmixer/")
+    (license gpl2+)))
 
 jack-mixer
