fa0c97571c7cab87e0a69e45aa2433be4ff89df9
Divya Ranjan <divya@subvertising.org>
2025-01-01 13:06:58 +0000

    Add divya-lambda channel and update guix-science URL

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,11 +1,21 @@
 ;;; Channels
 
 (cons*
+ ;; Divya-Lambda
+ (channel
+  (name 'divya-lambda)
+  (url "https://codeberg.org/divyaranjan/divya-lambda.git")
+  (branch "master")
+  (introduction
+   (make-channel-introduction
+    "fe2010125fcbe003de42436b1a73ab53cc5e8288"
+    (openpgp-fingerprint
+     "F0B3 1A69 8006 8FB8 096A  2F12 B245 10C6 108C 8D4A"))))
+
  ;; Guix Science
  (channel
   (name 'guix-science)
-  (url "https://github.com/guix-science/guix-science.git")
-  (branch "master")
+  (url "https://codeberg.org/guix-science/guix-science.git")
   (introduction
    (make-channel-introduction
     "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
@@ -33,4 +43,14 @@
     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
     (openpgp-fingerprint
      "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+
+ ;; Emacs Master
+ ;; (channel
+ ;;        (name 'emacs-master)
+ ;;        (url "https://codeberg.org/akib/guix-channel-emacs-master")
+ ;;        (introduction
+ ;;         (make-channel-introduction
+ ;;          "1ba8c40e21c1c18f70c8ff116f2fbbbb41a5a30a"
+ ;;          (openpgp-fingerprint
+ ;;           "C954 CA9A BB4B EA43 417B  7151 5535 FCF5 4D88 616B"))))
  %default-channels)
