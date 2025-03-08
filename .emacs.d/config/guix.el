ac8023e2e665fa7f4a0d787adbc61aae8c8209ab
Divya Ranjan <divya@subvertising.org>
2025-03-08 11:08:51 +0000

    Add emacs-guix through require

--- .emacs.d/config/guix.el
+++ .emacs.d/config/guix.el
@@ -4,10 +4,14 @@
 
 ;;; Code:
 
-(use-package guix
-  :straight t)
+;; (use-package guix
+;;   :straight t)
+(require 'guix)
 
 (global-set-key (kbd "C-x C-g") 'guix)
+
+;; For copyright signatures in guix checkouts
+(load-file "/mnt/code/big-src/guix/etc/copyright.el")
 
 (provide 'guix.el)
 ;;; guix.el ends here
