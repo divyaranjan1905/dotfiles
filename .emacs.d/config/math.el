194f74077f8e4219d892626face51f1f63478e85
Divya Ranjan <divya@subvertising.org>
2025-03-08 11:09:36 +0000

    Remove lean4-mode and lsp-mode

--- .emacs.d/config/math.el
+++ .emacs.d/config/math.el
@@ -4,17 +4,23 @@
 
 ;;; Proof General (Coq, et.al)
 (use-package proof-general
-  :straight t)
-
+  :straight t
+  :defer t)
 
 ;;; Lean4
-(use-package lean4-mode
-  :commands lean4-mode
-  :straight (lean4-mode :type git :host github
-                        :repo "leanprover/lean4-mode"
-                        :files ("*.el" "data")))
 
-(add-to-list 'exec-path "/home/divya/.elan/bin")
+;; (use-package lsp-mode
+;;   :straight t
+;;   :defer t)
+
+;; (use-package lean4-mode
+;;   :defer t
+;;   :commands lean4-mode
+;;   :straight (lean4-mode :type git :host github
+;;                         :repo "leanprover/lean4-mode"
+;;                         :files ("*.el" "data")))
+
+(add-to-list 'exec-path "/home/divya/.elan/bin/")
 
 (provide 'math)
 ;;; math.el ends here
