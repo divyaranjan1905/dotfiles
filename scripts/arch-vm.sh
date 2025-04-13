9ee6f8c36e20578e70d89386eb49b9dd31ad46df
Divya Ranjan <divya@subvertising.org>
2025-04-13 14:54:28 +0000

    Add arch-vm script

--- scripts/arch-vm.sh
+++ scripts/arch-vm.sh
@@ -3,5 +3,8 @@ qemu-system-x86_64 \
 -enable-kvm \
 -m 4G \
 -smp 4 \
--hda /mnt/LDisk-D/virtual-machines/arch-reverse.qcow2 \
--cdrom "/mnt/LDisk-E/Albert Einstein/THE UNIVERSE/Arch Linux/archlinux-2025.03.01-x86_64.iso"
+-vga virtio \
+-drive file="/mnt/LDisk-D/virtual-machines/arch-reverse.qcow2",format=qcow2 \
+-boot order=c \
+-netdev user,id=net0 -device e1000,netdev=net0
+# -cdrom "/mnt/LDisk-E/Albert Einstein/THE UNIVERSE/Arch Linux/archlinux-2025.03.01-x86_64.iso"
